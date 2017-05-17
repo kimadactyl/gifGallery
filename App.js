79ad5cc6ea38572ebcee3127f56744f747188f1c
Kim Foale <kim@alliscalm.net>
2017-05-17 18:30:46 +0100

    Get basic functionality working

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        <Image style={styles.image} source={{uri: 'http://gfsc.network/assets/images/phone.gif'}} />
+        <TouchableHighlight style={styles.touch}>
+          <View style={styles.touch}>
+            <ImageBox />
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
+}
+
+class ImageBox extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      imageURL: '',
+      isLoading: true
+    }
+  }
+
+  _getImage() {
+    fetch('http://192.168.1.116:4567/image.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // console.log(responseJson)
+        this.setState({
+          imageURL: responseJson.video,
+          isLoading: false
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  componentWillMount() {
+    this._getImage();
+    console.log(this.state.imageURL)
+  }
+
+  render() {
+    if(this.state.isLoading) {
+      return <Text>Loading...</Text>
+    } else {
+      return <Image style={styles.image} source={{uri: this.state.imageURL}} />
+    }
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -20,6 +64,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   image: {
+    flex: 1
+  },
+  touch: {
     flex: 1
   }
 });
